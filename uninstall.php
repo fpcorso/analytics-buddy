34584c532602481af71bc3b945a630e28e42c557
Frank Corso <frank@frankcorso.me>
2018-05-30 20:40:05 -0400

    Adds uninstall file

---
+++ uninstall.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is called when plugin is deleted.
+ *
+ * @package ANBU
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
+}
+
+delete_option( 'anbu-settings' );
+?>
